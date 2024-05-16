14b38f1b9f5bd85ada8e8c9886fd64cb16b61698
Darwin Gutierrez <you@example.com>
2024-05-16 12:01:28 -0600

    1.1

--- app/src/main/java/com/example/proyectobus/Login.kt
+++ app/src/main/java/com/example/proyectobus/Login.kt
@@ -13,9 +13,10 @@ class Login : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         Thread.sleep(300)
         installSplashScreen()
+        super.onCreate(savedInstanceState)
+
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
